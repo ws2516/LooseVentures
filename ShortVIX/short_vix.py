09df2ee3cd0d3a9adf271133724e16e5553c8c78
William Shamma <william.shamma2@gmail.com>
2021-05-29 18:09:58 -0400

    Initial Vix Done, to do hyperparam opt.

--- ShortVIX/short_vix.py
+++ ShortVIX/short_vix.py
@@ -2,41 +2,168 @@ import pandas as pd
 import numpy as np
 import altair as alt
 import matplotlib.pyplot as plt
+from datetime import date
+
+
+def get_hold_time(date1, date2):
+	d1split = date1.split('-')
+	d2split = date2.split('-')
+	d0 = date(int(d1split[0]), int(d1split[1]), int(d1split[2]))
+	d1 = date(int(d2split[0]), int(d2split[1]), int(d2split[2]))
+	delta = d1 - d0
+	return delta.days
 
 panel_data = pd.read_csv('./VIX.csv')
 
-date = panel_data['Date'].values
+dater = panel_data['Date'].values
 close_data = panel_data['Close'].values
 #adjusted_close = panel_data['Adj. Close'].values
 open_data = panel_data['Open'].values
 
-vix_gt = 30
-vix_incr_increase = 10
-vix_decreas_multi = 0.33
-buy_mult = 0.33
-track = []
-stack = ['Init']
-pos_star = 100 #100% of the share at that price
-buy_price = 10000000
+#------------------- parameters ---------------------
+vix_trigger = 30
+inc_increment = 10
+dec_target = 0.66
+pos_multipler = 0.33
+number_of_increments = 3
+posit = 10000
+
+#------------------- dummy variables and temps ---------------------
+buy_price = 1000000
+InitBuy = True
+transaction, BBmS, Date, transaction_number, transaction_value = [], [], [], [], []
+share_num , tester = [], []
+counted = 0
 
 for i in range(len(open_data)):
-	if close_data[i] >= 30 and stack[-1] not in ['Buy','Buy More']:
-		stack.append('Buy')
+	if close_data[i] >= vix_trigger and InitBuy:
+		counted += 1
+		transaction += [close_data[i]]
+		BBmS += ['Buy']
+		Date += [dater[i]]
 		buy_price = close_data[i]
-		position = close_data[i] #sell short
-	elif close_data[i]-buy_price >= 10 and stack[-1] not in ['Sell']:
-		stack.append('Buy More')
-		buy_price = (buy_price + close_data[i])/2
-		position = buy_price
-	elif close_data[i]/buy_price <= 0.66 and stack[-1] not in ['Sell']:
-		stack.append('Sell')
-		sell_price = -close_data[i]
-		position = buy_price+sell_price
+		transaction_number += [counted]
+		shares = (posit/number_of_increments)//buy_price
+		share_num += [shares]
+		transaction_value += [shares*buy_price]
+		InitBuy = False
+		counter = 1
+		
+	elif close_data[i]-buy_price >= inc_increment and not InitBuy and counter<number_of_increments:
+		transaction += [close_data[i]]
+		buy_price = (close_data[i]+counter*buy_price)/(counter+1) #fix this
+		counter += 1
+		BBmS += ['Buy More']
+		transaction_number += [counted]
+		shares += (posit/number_of_increments)//buy_price
+		share_num += [shares]
+		transaction_value += [shares*buy_price]
+		Date += [dater[i]]
+		
+	elif close_data[i]/buy_price <= dec_target and not InitBuy:
+		InitBuy = True
+		transaction += [close_data[i]]
+		BBmS += ['Sell']
+		Date += [dater[i]]
+		transaction_number += [counted]
+		transaction_value += [shares*close_data[i]]
+		share_num += [shares]
 	else:
 		continue
-	track.append(position)
 
-plt.plot(np.arange(len(track)),track)
-plt.show()
-	
-	
+#--------------------------------------------------
+
+df = pd.DataFrame({'Date':Date, 'Buy/BuyMore/Sell':BBmS, 'Price':transaction, 'Transaction Number':transaction_number, 'Transaction Value':transaction_value, "Number of Shares":share_num })
+#df.to_csv('./PaperLedger.csv')
+
+'''
+alt.Chart(df).mark_point(filled = True).encode(
+    x='Date:T',
+    y='Price:Q',
+    color='Buy/BuyMore/Sell:N'
+).show()
+'''
+
+#--------------------------------------------------
+returns, tnumber, pos_used = [], [], []
+for i in np.unique(df.TNumber.values):
+	newdf = df[df.TNumber == i]
+	returns += [(newdf['TValue'].values[-2] - newdf['TValue'].values[-1])/newdf['TValue'].values[-2]]
+	tnumber += [i]
+	pos_used += [round((len(newdf)-1)/number_of_increments,2)]
+
+print(sum(returns))
+'''
+alt.Chart(pd.DataFrame({'Transaction Number':tnumber, 'Returns':returns, 'Position Used':pos_used})).mark_bar().encode(
+    x='Transaction Number:O',
+    y='Returns:Q',
+    color='Position Used:O'
+).show()
+'''
+
+#--------------------------------------------------
+holdTime, datesHeld, startHold = [], [], []
+
+df = df[df['Buy/BuyMore/Sell'] != 'Buy More']
+for i in range(len(df.Date.values)//2):
+	holdTime += [get_hold_time(df.Date.values[2*i],df.Date.values[2*i + 1])]
+	datesHeld += [str(df.Date.values[2*i]) + ' to ' + str(df.Date.values[2*i + 1])]
+	startHold += [df.Date.values[2*i]]
+
+'''
+alt.Chart(pd.DataFrame({'datesHeld':datesHeld, 'holdTime':holdTime, 'Start':startHold})).mark_bar().encode(
+    x='Start:T',
+    y='holdTime:Q',
+    tooltip=['datesHeld']
+).interactive().show()
+'''
+
+'''
+Trade Stats: (On Close)
+
+Hold Time between Buy and Sell: Mean 73 Days, Median 49 Days, Max 246, Min 3
+
+Increase Position Frequency: ~23%
+
+Percent Profitable: 100%
+
+Average Profit per Trade: $12.9 on a base of one unit trades (~$30 entry)
+
+Median Profit per Trade: $12.1 on a base of one unit trades (~$30 entry)
+
+
+
+
+
+
+Trade Stats: (On Open)
+
+Average Hold Time between Buy and Sell:
+
+Increase Position Frequency:
+
+Percent Profitable:
+
+Average Profit per Trade:
+
+Median Profit per Trade:
+
+
+
+
+
+
+Trade Stats: (On Adjusted Close)
+
+Average Hold Time between Buy and Sell:
+
+Increase Position Frequency:
+
+Percent Profitable:
+
+Average Profit per Trade:
+
+Median Profit per Trade:
+
+
+'''
