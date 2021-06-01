24c16468fd611c475620d8acd0c01481a677bd21
William Shamma <william.shamma2@gmail.com>
2021-06-01 17:10:36 -0400

    Simulation complete

--- ShortVIX/short_vix.py
+++ ShortVIX/short_vix.py
@@ -21,11 +21,10 @@ close_data = panel_data['Close'].values
 open_data = panel_data['Open'].values
 
 #------------------- parameters ---------------------
-vix_trigger = 30
-inc_increment = 10
-dec_target = 0.66
-pos_multipler = 0.33
-number_of_increments = 3
+vix_triggers = np.arange(30,50)[::-1]
+inc_increments = np.arange(5,10)
+dec_targets = np.linspace(0.50,0.75, num=5)
+number_of_increments_list = np.arange(3,6)
 posit = 10000
 
 #------------------- dummy variables and temps ---------------------
@@ -34,47 +33,73 @@ InitBuy = True
 transaction, BBmS, Date, transaction_number, transaction_value = [], [], [], [], []
 share_num , tester = [], []
 counted = 0
+cumulative_returns, number_of_trades, mean_return, max_return, median_return, vix_triggered, inc_incremented, dec_targeted, number_of_incremented = [],[],[],[],[],[],[],[],[]
 
-for i in range(len(open_data)):
-	if close_data[i] >= vix_trigger and InitBuy:
-		counted += 1
-		transaction += [close_data[i]]
-		BBmS += ['Buy']
-		Date += [dater[i]]
-		buy_price = close_data[i]
-		transaction_number += [counted]
-		shares = (posit/number_of_increments)//buy_price
-		share_num += [shares]
-		transaction_value += [shares*buy_price]
-		InitBuy = False
-		counter = 1
-		
-	elif close_data[i]-buy_price >= inc_increment and not InitBuy and counter<number_of_increments:
-		transaction += [close_data[i]]
-		buy_price = (close_data[i]+counter*buy_price)/(counter+1) #fix this
-		counter += 1
-		BBmS += ['Buy More']
-		transaction_number += [counted]
-		shares += (posit/number_of_increments)//buy_price
-		share_num += [shares]
-		transaction_value += [shares*buy_price]
-		Date += [dater[i]]
-		
-	elif close_data[i]/buy_price <= dec_target and not InitBuy:
-		InitBuy = True
-		transaction += [close_data[i]]
-		BBmS += ['Sell']
-		Date += [dater[i]]
-		transaction_number += [counted]
-		transaction_value += [shares*close_data[i]]
-		share_num += [shares]
-	else:
-		continue
+for vix_trigger in vix_triggers:
+    for inc_increment in inc_increments:
+        for dec_target in dec_targets:
+            for number_of_increments in number_of_increments_list:
+                for i in range(len(open_data)):
+                    if close_data[i] >= vix_trigger and InitBuy:
+                        counted += 1
+                        transaction += [close_data[i]]
+                        BBmS += ['Buy']
+                        Date += [dater[i]]
+                        buy_price = close_data[i]
+                        transaction_number += [counted]
+                        shares = (posit/number_of_increments)//buy_price
+                        share_num += [shares]
+                        transaction_value += [shares*buy_price]
+                        InitBuy = False
+                        counter = 1
+                    elif close_data[i]-buy_price >= inc_increment and not InitBuy and counter<number_of_increments:
+                        transaction += [close_data[i]]
+                        buy_price = (close_data[i]+counter*buy_price)/(counter+1) #fix this
+                        counter += 1
+                        BBmS += ['Buy More']
+                        transaction_number += [counted]
+                        shares += (posit/number_of_increments)//buy_price
+                        share_num += [shares]
+                        transaction_value += [shares*buy_price]
+                        Date += [dater[i]]
+                    elif close_data[i]/buy_price <= dec_target and not InitBuy:
+                        InitBuy = True
+                        transaction += [close_data[i]]
+                        BBmS += ['Sell']
+                        Date += [dater[i]]
+                        transaction_number += [counted]
+                        transaction_value += [shares*close_data[i]]
+                        share_num += [shares]
+                    else:
+                        continue
+                df = pd.DataFrame({'Date':Date, 'Buy/BuyMore/Sell':BBmS, 'Price':transaction, 'Transaction Number':transaction_number, 'Transaction Value':transaction_value, "Number of Shares":share_num }).round(2)
+                try:
+                	print(vix_trigger, inc_increment, dec_target, number_of_increments)
+                	returns, tnumber, pos_used = [], [], []
+                	for i in np.unique(df['Transaction Number'].values):
+                		newdf = df[df['Transaction Number'] == i]
+                		returns += [(newdf['Transaction Value'].values[-2] - newdf['Transaction Value'].values[-1])/newdf['Transaction Value'].values[-2]]
+                		tnumber += [i]
+                		pos_used += [round((len(newdf)-1)/number_of_increments,2)]
+                	cumulative_returns += [sum(returns)*100]
+                	number_of_trades += [len(returns)]
+                	mean_return += [np.mean(returns)]
+                	median_return += [np.median(returns)]
+                	max_return += [np.max(returns)]
+                	vix_triggered += [vix_trigger]
+                	inc_incremented += [inc_increment]
+                	dec_targeted += [dec_target]
+                	number_of_incremented += [number_of_increments]
+                except:
+                	print(df, vix_trigger, inc_increment, dec_target, number_of_increments)
+                	continue
+dfOpt = pd.DataFrame({'cumulative_returns':cumulative_returns, 'number_of_trades':number_of_trades, 'mean_return':mean_return, 'median_return':median_return, 'max_return':max_return, "vix_triggered":vix_triggered, 'inc_incremented':inc_incremented,'dec_targeted':dec_targeted, 'number_of_incremented':number_of_incremented}).round(2)
+dfOpt.to_csv('./OptimizationReturnsLedger.csv')           
 
 #--------------------------------------------------
 
-df = pd.DataFrame({'Date':Date, 'Buy/BuyMore/Sell':BBmS, 'Price':transaction, 'Transaction Number':transaction_number, 'Transaction Value':transaction_value, "Number of Shares":share_num }).round(2)
-df.to_csv('./PaperLedger.csv')
+#df = pd.DataFrame({'Date':Date, 'Buy/BuyMore/Sell':BBmS, 'Price':transaction, 'Transaction Number':transaction_number, 'Transaction Value':transaction_value, "Number of Shares":share_num }).round(2)
+'''df.to_csv('./PaperLedger.csv')
 
 
 alt.Chart(df).mark_point(filled = True, size = 30).encode(
@@ -117,7 +142,7 @@ alt.Chart(pd.DataFrame({'datesHeld':datesHeld, 'holdTime':holdTime, 'Start':star
     y='holdTime:Q',
     tooltip=['Start','datesHeld','holdTime']
 ).interactive().show()
-
+'''
 
 '''
 Trade Stats: (On Close)
